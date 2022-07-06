e1646908c0b3e3748dee31bf41db5d1d88278f13
Skye Chen <skyechen28@gmail.com>
2022-07-06 03:22:55 -0400

    Added licence

--- htmlvalidation.py
+++ htmlvalidation.py
@@ -1,6 +1,33 @@
+'''
+    This code was copied and modified from Sudoblark/w3c-html-python-validator on GitHub:
+    https://github.com/Sudoblark/w3c-html-python-validator/blob/main/src/HTMLValidator.py
+    
+    Sudoblark/w3c-html-python-validator is licensed under the   
+    MIT License
+
+    Copyright (c) 2021 Othneil Drew
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+'''
+
 from urllib import request
 import json
-
 
 class HTMLValidator:
     def __init__(self):
